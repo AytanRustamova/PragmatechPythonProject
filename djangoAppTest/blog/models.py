41696403681afba09a3b8b3c7c6f036768a9f819
AytanRustamova <aytn1973@gmail.com>
2021-11-01 18:06:44 +0300

    integrated many to many reletions to database

--- djangoAppTest/blog/models.py
+++ djangoAppTest/blog/models.py
@@ -15,12 +15,21 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.CharField(max_length=63, verbose_name='Muellif', )
     cats = models.IntegerField(max_length=100, choices=CATS, default=1)
+    blog_tags = models.ManyToManyField('Tag')
+
 
 
     class Meta:
         verbose_name = "Blog"
         verbose_name_plural = "Blogs"
         ordering = ('created_at',)
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=63, null=True, blank=True)
+
     def __str__(self):
         return self.title
 
