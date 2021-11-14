a540ade25d6ac08a391d8745760518e216ef9e15
AytanRustamova <aytn1973@gmail.com>
2021-11-14 15:31:57 +0300

    custom-filter, custom-tag

--- djangoAppTest/user/models.py
+++ djangoAppTest/user/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=127, null=True, blank=True)
     email = models.TextField(help_text='Bura emailinizi daxil edin...')
-    password = models.(auto_now_add=True)
+    
     confPassword = models.CharField(max_length=63, verbose_name='Muellif', )
 
 
