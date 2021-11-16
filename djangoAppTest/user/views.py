ec77a6ca237be594c0362679d6a6966cba3ed723
AytanRustamova <aytn1973@gmail.com>
2021-11-16 16:58:50 +0300

    register form is valid

--- djangoAppTest/user/views.py
+++ djangoAppTest/user/views.py
@@ -1,15 +1,25 @@
 # User Views
 
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 
 
 def login(request):
+    context={}
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        
+        
+    context ={"form":form}
+    return render(request, 'register.html', context)
 
 
